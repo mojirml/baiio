a1cba61fd6dfe376918d4eea58954c412aaeace3
Bayen <wx_d80b4e30c8cd40ce8509f96c16d1e654@git.code.tencent.com>
2021-05-01 21:53:32 +0800

    11

---
+++ log/main.go
@@ -0,0 +1,15 @@
+package log
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"log"
+)
+
+func main() {
+	uuids := uuid.NewV1()
+	log.SetPrefix(uuids.String() + " ")
+	log.SetFlags(log.Ldate | log.Lshortfile)
+	log.Print("我就是一条日志")
+	log.Printf("%s,", "谁说我是日志了，我是错误")
+
+}
