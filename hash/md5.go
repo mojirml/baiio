62363a001f3de459b7e67250d3dd3f429a44d34c
Bayen <wx_d80b4e30c8cd40ce8509f96c16d1e654@git.code.tencent.com>
2021-05-28 10:34:29 +0800

    22

--- hash/md5.go
+++ hash/md5.go
@@ -1,12 +1,12 @@
 package hash
 
 import (
-"crypto/md5"
-"encoding/hex"
-"errors"
-"fmt"
-"io"
-"os"
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 // return file md5 and error
@@ -30,4 +30,8 @@ func StringMd5(s string) string {
 	md5 := md5.New()
 	md5.Write([]byte(s))
 	return hex.EncodeToString(md5.Sum(nil))
+}
+
+func init() {
+	fmt.Println("init md5")
 }
--- log/main.go
+++ log/main.go
@@ -1,6 +1,7 @@
-package log
+package main
 
 import (
+	"fmt"
 	uuid "github.com/satori/go.uuid"
 	"log"
 )
@@ -11,5 +12,8 @@ func main() {
 	log.SetFlags(log.Ldate | log.Lshortfile)
 	log.Print("我就是一条日志")
 	log.Printf("%s,", "谁说我是日志了，我是错误")
+}
 
+func init() {
+	fmt.Println("init main")
 }
